494c9aeb9586b70cfee534a3e028d54bd2bc310f
Kejia Wu <kjwu3@hotmail.com>
2021-03-28 02:18:30 +1100

    change Nav text based on url

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Layout({ children }) {
+    const location = useLocation();
+    const { pathname } = location;
+
     return (
         <>
             <nav className="navbar navbar-dark bg-primary">
-                <a className="navbar-brand" href="/">Navbar</a>
+            <a className="navbar-brand" href="/">{pathname.includes('/user')?'User Details':'Home'}</a>
             </nav>
             <div className="container pt-3">
                 { children }
