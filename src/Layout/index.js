08d8341f6d5be9bef057ad871e006c5507242e75
Kejia Wu <kjwu3@hotmail.com>
2021-03-28 12:15:16 +1100

    and back arrow

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
     return (
         <>
             <nav className="navbar navbar-dark bg-primary">
-            <a className="navbar-brand" href="/">{pathname.includes('/user')?'User Details':'Home'}</a>
+            <a className="navbar-brand" href="/">{pathname.includes('/user')?'◀︎User Details':'Home'}</a>
             </nav>
             <div className="container pt-3">
                 { children }
