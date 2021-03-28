736a69b2217a5bf594129c67830c0ba6bea81c8d
Kejia Wu <kjwu3@hotmail.com>
2021-03-28 12:14:23 +1100

    updated input component

--- src/component/Input/index.js
+++ src/component/Input/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-function Input({name, label, type, placeholder = '', errors, readOnly }, ref) {
+function Input({ name, label, type, placeholder = '', errors, readOnly }, ref) {
     return (
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
-            <input type={type??'text'} name={name} ref={ref} className={`form-control ${errors[name]?'is-invalid':''}`} placeholder={placeholder} readOnly={readOnly} data-testid={`${name}-input`}/>
+            <input 
+                type={type ?? 'text'} 
+                name={name} ref={ref} 
+                className={`form-control ${errors[name] ? 'is-invalid' : ''}`} 
+                placeholder={placeholder} 
+                readOnly={readOnly} 
+                data-testid={`${name}-input`} />
             { errors[name] && <div className="invalid-feedback">{'error'}</div>}
         </div>
     )
